2cc3c41de0b07d21ee8555294312495b0968b37a
Andrew Rumiantsev <andy1116286@gmail.com>
2017-12-05 11:35:47 +0300

    some small changes

--- breakpointSearcher.py
+++ breakpointSearcher.py
@@ -11,7 +11,7 @@ import theano.tensor as T
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
 #breakpoint search
-accurate_result=np.matrix([100,4,5,9,7]).T
+accurate_result=np.matrix([100,4,5,7]).T
 def modulateRegression(regressionSampleQuintity,regressionOutlierPercentage):
     regressionParameters = accurate_result #надо брать свободный член больше
     x_points = np.zeros(shape=[regressionSampleQuintity,len(regressionParameters)])
@@ -28,20 +28,20 @@ def modulateRegression(regressionSampleQuintity,regressionOutlierPercentage):
             x_points[i] = np.append(np.ones(1),np.random.uniform(-5,5,size = len(regressionParameters)-1))
             y_points[i]=np.random.normal(100,10, size=1)
     return (x_points,y_points)
-cycleOulierPercentage = 5
+cycleOulierPercentage = 1
 while cycleOulierPercentage<100:
     print("Going to perform test with percentage {0:d}%....".format(cycleOulierPercentage))
     x_points,y_points=modulateRegression(100,cycleOulierPercentage)
-    APPROXIMATION_MODEL=sm.RLM(y_points,x_points,M=sm.robust.norms.HuberT())
+    APPROXIMATION_MODEL=sm.RLM(y_points,x_points, M=sm.robust.norms.HuberT())
     tempHundredParams=APPROXIMATION_MODEL.fit().params
-    print(tempHundredParams)
+    print(accurate_result.T-tempHundredParams)
 
-    x_points,y_points=modulateRegression(3000,cycleOulierPercentage)
-    APPROXIMATION_MODEL = sm.RLM(y_points,x_points,M=sm.robust.norms.HuberT())
+    x_points,y_points=modulateRegression(10000,cycleOulierPercentage)
+    APPROXIMATION_MODEL = sm.RLM(y_points,x_points, M=sm.robust.norms.HuberT())
     tempThousandParams = APPROXIMATION_MODEL.fit().params
-    print(tempThousandParams)
+    print(accurate_result.T-tempThousandParams)
     if np.linalg.norm(accurate_result-tempHundredParams)<=np.linalg.norm(accurate_result-tempThousandParams):
         print("Breakpoint for this approximation model is:{0:d}%".format(cycleOulierPercentage))
         break
-    cycleOulierPercentage +=5
+    cycleOulierPercentage +=1
 
