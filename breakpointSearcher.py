5d9e771c20cb5d28e03c0a08ae3acb0ae2df4985
Andrew Rumiantsev <andy1116286@gmail.com>
2018-03-15 22:37:00 +0300

    some more productivity updates

--- breakpointSearcher.py
+++ breakpointSearcher.py
@@ -13,7 +13,7 @@ import statsmodels.formula.api as smf
 from multiprocessing import Process, Queue , Pipe
 #breakpoint search
 accurate_result=np.matrix([100,4]).T
-N_MonteCarlo = 30
+N_MonteCarlo = 20
 epsilon = 0.01
 deltasHundred = np.zeros(1)
 deltasThousand = np.zeros(1)
@@ -35,13 +35,22 @@ def modulateRegression(regressionSampleQuintity,regressionOutlierPercentage):
             y_points[i]=np.random.normal(100,10, size=1)
     return (x_points,y_points)
 cycleOulierPercentage = 1.0
-def rlmForHundred(conn, cycleOulierPercentage):
+def rlmForHundred(conn, cycleOulierPercentage, hundredAndThousandConn_send):
     x_points,y_points=modulateRegression(1000,cycleOulierPercentage)
+    # hundredAndThousandConn_send.send(x_points)
+    # hundredAndThousandConn_send.send(y_points)
     APPROXIMATION_MODEL=sm.RLM(y_points,x_points, M=sm.robust.norms.HuberT())
     tempHundredParams=APPROXIMATION_MODEL.fit().params
     conn.send(tempHundredParams)
-def rlmForThousand(conn, cycleOulierPercentage):
-    x_points,y_points=modulateRegression(3000,cycleOulierPercentage)
+def rlmForThousand(conn, cycleOulierPercentage, hundredAndThousandConn_recv):
+    # x_points = hundredAndThousandConn_recv.recv()
+    # y_points = hundredAndThousandConn_recv.recv()
+    # temp_x_points,temp_y_points=modulateRegression(2000,cycleOulierPercentage)
+    # x_points = np.append(x_points, temp_x_points)
+    # y_points = np.append(y_points, temp_y_points)
+    # plt.plot(x_points,y_points,'ro')
+    # plt.show()
+    x_points,y_points=modulateRegression(2000,cycleOulierPercentage)
     APPROXIMATION_MODEL = sm.RLM(y_points,x_points, M=sm.robust.norms.HuberT())
     tempThousandParams = APPROXIMATION_MODEL.fit().params
     conn.send(tempThousandParams)
@@ -52,23 +61,26 @@ while cycleOulierPercentage<100:
     for i in range(1,int(N_MonteCarlo+1)):
         parent_conn_hundred, child_conn_hundred = Pipe()
         parent_conn_thousand, child_conn_thousand = Pipe()
-        pHundred = Process(target=rlmForHundred, args=(child_conn_hundred, cycleOulierPercentage))
-        pThousand = Process(target=rlmForThousand, args=(child_conn_thousand,cycleOulierPercentage))
+        hundredAndThousandConn_recv, hundredAndThousandConn_send = Pipe()
+        pHundred = Process(target=rlmForHundred, args=(child_conn_hundred, cycleOulierPercentage, hundredAndThousandConn_send))
+        pThousand = Process(target=rlmForThousand, args=(child_conn_thousand,cycleOulierPercentage, hundredAndThousandConn_recv))
         pHundred.start()
         pThousand.start()
         # x_points,y_points=modulateRegression(1000,cycleOulierPercentage)
         # APPROXIMATION_MODEL=sm.RLM(y_points,x_points, M=sm.robust.norms.HuberT())
         tempHundredParams= parent_conn_hundred.recv()
         tempThousandParams = parent_conn_thousand.recv()
-        pHundred.join()
-        pThousand.join()
         discrepancyHundred += np.linalg.norm(np.squeeze(np.asarray(accurate_result.T-tempHundredParams)))
         # x_points,y_points=modulateRegression(3000,cycleOulierPercentage)
         # APPROXIMATION_MODEL = sm.RLM(y_points,x_points, M=sm.robust.norms.HuberT())
         # tempThousandParams = APPROXIMATION_MODEL.fit().params
         discrepancyThousand += np.linalg.norm(np.squeeze(np.asarray(accurate_result.T-tempThousandParams)))
+        # pHundred.join()
+        # pThousand.join()
         # plt.plot(x_points.T[1],y_points,'ro')
         # plt.show()
+        pHundred.terminate()
+        pThousand.terminate()
     discrepancyHundred/=N_MonteCarlo
     discrepancyThousand/=N_MonteCarlo
     deltasHundred = np.append(deltasHundred, discrepancyHundred)
