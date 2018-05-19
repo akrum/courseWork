988406a940c72ee3f4d7282a7e168f7c6fff7976
Andrew Rumiantsev <andy1116286@gmail.com>
2018-05-19 15:14:37 +0300

    made dichotomy method

--- groupingEstimates.py
+++ groupingEstimates.py
@@ -26,9 +26,11 @@ class approximationGEMModel:
         temp/= self.erf((a_mu_i_plus_1-x_i*beta_hat)/(math.sqrt(2.0)*self.sigmasq))-self.erf((a_mu_i-x_i*beta_hat)/(math.sqrt(2.0)*self.sigmasq))
         return x_i*temp.item((0,0))
     def dlikelihood_f(self, beta):
-        tempValue=np.zeros(beta.size)
-        # tempValue[0] =
-        return 0.0 
+        current_likelihood_derivative_result = 0.0
+        for i in range(0, self.endogen.size):
+            current_likelihood_derivative_result+= self.dP(self.exogen[i], self.endogen[i], self.mu_data[i], beta)
+        # print current_likelihood_derivative_result
+        return current_likelihood_derivative_result
     def __init__(self, exogen_data, endogen_data):
         self.a = 8.0/(3.0*math.pi)*(3.0-math.pi)/(math.pi-4.0)
         self.interval_length = 1.0
@@ -45,17 +47,39 @@ class approximationGEMModel:
         # print int(round(-2.3/self.interval_length))
     def reclassificate(self):
         return 0
+    def dichotomy(self, beta_0, beta_1, possible_beta_hats, psi):
+        tempComparisonResult = np.less(self.dlikelihood_f(beta_0).T*self.dlikelihood_f(beta_1),np.zeros(self.exogen[0].size).T)
+        print tempComparisonResult
+        # assert tempComparisonResult[0].all()
+        if(np.less(np.abs(beta_0-beta_1),2*psi)[0].all()):
+            possible_beta_hats.append(beta_0)
+            return
+        beta_2 = 0.5*(beta_0+beta_1)
+        # tempComparisonResult = np.less(self.dlikelihood_f(beta_1).T*self.dlikelihood_f(beta_0),np.zeros(self.exogen[0].size).T)
+        # for i in range(0, tempComparisonResult[0].size):
+        #     if tempComparisonResult[0][i]==False:
+        #         beta_2[i]= 0.5*(beta_0[i]+beta_1[i])
+        
+        del tempComparisonResult
+        tempComparisonResult = np.less(self.dlikelihood_f(beta_2).T*self.dlikelihood_f(beta_0),np.zeros(self.exogen[0].size).T)
+        if tempComparisonResult[0].any():
+            self.dichotomy(beta_2,beta_0,possible_beta_hats,psi)
+        
+        del tempComparisonResult
+        tempComparisonResult = np.less(self.dlikelihood_f(beta_2).T*self.dlikelihood_f(beta_1),np.zeros(self.exogen[0].size).T)
+        if tempComparisonResult[0].any():
+            self.dichotomy(beta_2,beta_1,possible_beta_hats,psi)
+        
+        
+        
     def fit(self):
         self.classificate()
         beta_hat=np.matrix(np.ones(self.exogen[0].size)).T
-        current_likelihood_derivative_result = 0.0
-        for i in range(0, self.endogen.size):
-            current_likelihood_derivative_result+= self.dP(self.exogen[i], self.endogen[i], self.mu_data[i], beta_hat)
-        print current_likelihood_derivative_result
-        current_likelihood_derivative_result = 0.0
-        for i in range(0, self.endogen.size):
-            current_likelihood_derivative_result+= self.dP(self.exogen[i], self.endogen[i], self.mu_data[i], accurate_result)
-        print current_likelihood_derivative_result
+        print self.dlikelihood_f(beta_hat)
+        print self.dlikelihood_f(accurate_result)
+        dichotomyPossibleBetaHats = []
+        self.dichotomy(np.matrix(np.full(self.exogen[0].size, -200)).T,np.matrix(np.full(self.exogen[0].size, 200)).T,dichotomyPossibleBetaHats, np.full(self.exogen[0].size, 0.0001))
+        print(dichotomyPossibleBetaHats)
         return np.zeros(self.exogen.size)
 def GEM(exogen_data, endogen_data, *args):
     return approximationGEMModel(exogen_data,endogen_data)
@@ -76,7 +100,7 @@ def modulateRegression(regressionSampleQuintity,regressionOutlierPercentage):
             y_points[i]=np.random.normal(100,10, size=1)
     return (x_points,y_points)
 
-x_points,y_points=modulateRegression(10, 1.0)
+x_points,y_points=modulateRegression(100, 1.0)
 APPROXIMATION_MODEL = GEM(x_points,y_points)
 print APPROXIMATION_MODEL.fit()
 # APPROXIMATION_MODEL=sm.RLM(y_points,x_points, M=sm.robust.norms.HuberT())
