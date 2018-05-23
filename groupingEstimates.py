36ab74de0566a4e42d09c9e620e5000c8a4c5bbc
Andrew Rumiantsev <andy1116286@gmail.com>
2018-05-24 00:02:56 +0300

    Got some pretty good results in dlikelyhood

--- groupingEstimates.py
+++ groupingEstimates.py
@@ -4,12 +4,25 @@ import math
 from multiprocessing import Process, Queue , Pipe
 from thread import start_new_thread
 import threading
-accurate_result=np.matrix([100,4]).T
+import scipy.special
+accurate_result=np.matrix([90,4]).T
 epsilon = 0.01
 class approximationGEMModel:
+    def __init__(self, exogen_data, endogen_data):
+        self.a = 8.0/(3.0*math.pi)*(3.0-math.pi)/(math.pi-4.0)
+        self.interval_length = 0.0001
+        self.k = 100000000
+        self.intervals_left_bound = 0.0-self.interval_length*self.k/2.0
+        self.intervals_right_bound = 0.0+self.interval_length*self.k/2.0
+        self.exogen=exogen_data
+        self.endogen=endogen_data
+        self.sigmasq= 225.0
+        self.threadingEvent = threading.Event()
     def erf(self, value):
         # print "{0:f}\n".format(math.sqrt(1.0-math.exp((-1.0*value*value)*(4.0/math.pi+self.a*value*value)/(1.0+self.a*value*value))))
         return math.sqrt(1.0-math.exp((-1.0*value*value)*(4.0/math.pi+self.a*value*value)/(1.0+self.a*value*value)))
+        
+        # return scipy.special.erf(value)
     def derf(self, value):
         temp=math.exp((-1*value*value)*(4/math.pi+self.a*value*value)/(1+self.a*value*value))
         temp*=((-2*value+2*self.a*value*value*value)*(4/math.pi+self.a*value*value)/(1+self.a*value*value)-2*self.a*value*value*value/(1+self.a*value*value))
@@ -17,25 +30,25 @@ class approximationGEMModel:
         return temp
     def P(self, x_i, y_i, mu_i, beta_hat):
         if mu_i==0:
-            return 0.5*(1+self.erf((self.intervals_left_bound-x_i*beta_hat)/(math.sqrt(2.0)*self.sigmasq)))
+            return 0.5*(1+self.erf((self.intervals_left_bound-x_i*beta_hat)/(math.sqrt(2.0*self.sigmasq))))
         if mu_i==self.k-1:
-            return 0.5*(1+self.erf((self.intervals_right_bound-x_i*beta_hat)/(math.sqrt(2.0)*self.sigmasq)))
-        a_mu_i_plus_1 = mu_i/2*self.interval_length+self.interval_length
-        a_mu_i = mu_i/2*self.interval_length+0.0
-        return 0.5*(self.erf((a_mu_i_plus_1-x_i*beta_hat)/(math.sqrt(2.0)*self.sigmasq))-self.erf((a_mu_i-x_i*beta_hat)/(math.sqrt(2.0)*self.sigmasq)))
+            return 0.5*(1+self.erf((self.intervals_right_bound-x_i*beta_hat)/(math.sqrt(2.0*self.sigmasq))))
+        a_mu_i_plus_1 = mu_i*self.interval_length
+        a_mu_i = mu_i*self.interval_length-self.interval_length
+        return 0.5*(self.erf((a_mu_i_plus_1-x_i*beta_hat)/(math.sqrt(2.0*self.sigmasq)))-self.erf((a_mu_i-x_i*beta_hat)/(math.sqrt(2.0*self.sigmasq))))
     def dP(self, x_i, y_i, mu_i, beta_hat):
         if mu_i==0:
-            return self.derf((self.intervals_left_bound-x_i*beta_hat)/(math.sqrt(2)*self.sigmasq))/(1.0+self.erf((self.intervals_left_bound-x_i*beta_hat)/(math.sqrt(2)*self.sigmasq)))
+            return 2.0*x_i*self.derf((self.intervals_left_bound-x_i*beta_hat)/(math.sqrt(2.0*self.sigmasq)))/(1.0+self.erf((self.intervals_left_bound-x_i*beta_hat)/(math.sqrt(2.0*self.sigmasq)))).item((0,0))
         if mu_i==self.k-1:
-            return self.derf((self.intervals_right_bound-x_i*beta_hat)/(math.sqrt(2)*self.sigmasq))/(1.0+self.erf((self.intervals_left_bound-x_i*beta_hat)/(math.sqrt(2)*self.sigmasq)))
-        a_mu_i_plus_1 = mu_i/2*self.interval_length+self.interval_length
-        a_mu_i = mu_i/2*self.interval_length+0.0
-        if self.erf((a_mu_i_plus_1-x_i*beta_hat)/(math.sqrt(2.0)*self.sigmasq))-self.erf((a_mu_i-x_i*beta_hat)/(math.sqrt(2.0)*self.sigmasq))==0.0:
+            return 2.0*x_i*self.derf((self.intervals_right_bound-x_i*beta_hat)/(math.sqrt(2.0*self.sigmasq)))/(1.0+self.erf((self.intervals_left_bound-x_i*beta_hat)/(math.sqrt(2.0*self.sigmasq)))).item((0,0))
+        a_mu_i_plus_1 = mu_i*self.interval_length
+        a_mu_i = mu_i*self.interval_length-self.interval_length
+        if self.erf((a_mu_i_plus_1-x_i*beta_hat)/(math.sqrt(2.0*self.sigmasq)))-self.erf((a_mu_i-x_i*beta_hat)/(math.sqrt(2.0*self.sigmasq)))==0.0:
             return 0
         temp = 0.0
-        temp+= self.derf((a_mu_i_plus_1-x_i*beta_hat)/(math.sqrt(2.0)*self.sigmasq))-self.derf((a_mu_i-x_i*beta_hat)/(math.sqrt(2.0)*self.sigmasq))
-        temp/= self.erf((a_mu_i_plus_1-x_i*beta_hat)/(math.sqrt(2.0)*self.sigmasq))-self.erf((a_mu_i-x_i*beta_hat)/(math.sqrt(2.0)*self.sigmasq))
-        return x_i*temp.item((0,0))
+        temp+= self.derf((a_mu_i_plus_1-x_i*beta_hat)/(math.sqrt(2.0*self.sigmasq)))-self.derf((a_mu_i-x_i*beta_hat)/(math.sqrt(2.0*self.sigmasq)))
+        temp/= self.erf((a_mu_i_plus_1-x_i*beta_hat)/(math.sqrt(2.0*self.sigmasq)))-self.erf((a_mu_i-x_i*beta_hat)/(math.sqrt(2.0*self.sigmasq)))
+        return 2.0*x_i*temp.item((0,0))
     
     def likelihood_f(self, beta):
         current_likelihood_result = 0.0
@@ -47,21 +60,12 @@ class approximationGEMModel:
         for i in range(0, self.endogen.size):
             current_likelihood_derivative_result+= self.dP(self.exogen[i], self.endogen[i], self.mu_data[i], beta)
         # print current_likelihood_derivative_result
-        return current_likelihood_derivative_result
-    def __init__(self, exogen_data, endogen_data):
-        self.a = 8.0/(3.0*math.pi)*(3.0-math.pi)/(math.pi-4.0)
-        self.interval_length = 1.0
-        self.k = 100000
-        self.intervals_left_bound = 0.0-self.interval_length*self.k/2.0
-        self.intervals_right_bound = 0.0+self.interval_length*self.k/2.0
-        self.exogen=exogen_data
-        self.endogen=endogen_data
-        self.sigmasq= 16.0
-        self.threadingEvent = threading.Event()
+        return -0.5*current_likelihood_derivative_result
     def classificate(self):
         self.mu_data = np.zeros(self.endogen.size)
         for i in range(0, self.endogen.size):
             self.mu_data[i] = int(round(self.endogen[i]/self.interval_length))
+        print (self.endogen[0],self.mu_data[0])
         # print int(round(-2.3/self.interval_length))
     def reclassificate(self):
         return 0
@@ -111,15 +115,16 @@ class approximationGEMModel:
         
     def gradient(self, beta_0,possible_betas):
         nabla_beta = self.dlikelihood_f(beta_0).reshape((2,1))
-        beta_new = beta_0-0.9*nabla_beta
-        # print (self.likelihood_f(beta_new),self.likelihood_f(beta_0))
-        if np.less(np.abs(beta_0-beta_new),np.full(beta_0.size,1e-3)).all():
+        beta_new = beta_0-nabla_beta
+        # print beta_new
+        if np.less(np.abs(beta_0-beta_new),np.full(beta_0.size,1e-1)).all():
             possible_betas.append(beta_new)
             self.threadingEvent.set()
             return 0
         else:
             gradientThread = threading.Thread(target=self.gradient, args=(beta_new, possible_betas))
             gradientThread.start()
+    
     def fit(self):
         self.classificate()
         beta_hat=np.matrix(np.ones(self.exogen[0].size)).T
@@ -130,7 +135,7 @@ class approximationGEMModel:
         # print(dichotomyPossibleBetaHats)
         current_beta = np.matrix(np.full(self.exogen[0].size, 100)).T
         temp_possible_betas = []
-        self.gradient(current_beta,temp_possible_betas)
+        self.gradient(accurate_result,temp_possible_betas)
         self.threadingEvent.wait()
         print temp_possible_betas
         self.threadingEvent.clear()
@@ -151,7 +156,7 @@ def modulateRegression(regressionSampleQuintity,regressionOutlierPercentage):
             y_points[i]=(x_points[i]*regressionParameters)+np.random.normal(0,4)
         else:
             x_points[i] = np.append(np.ones(1),np.random.uniform(-5,5,size = len(regressionParameters)-1))
-            y_points[i]=np.random.normal(100,10, size=1)
+            y_points[i]=np.random.normal(100.0,15.0, size=1)
     return (x_points,y_points)
 
 x_points,y_points=modulateRegression(100, 1.0)
