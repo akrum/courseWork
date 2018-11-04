4f5f215d2a36e624cc2663d4ccfad24aaddd578f
Andrew Rumiantsev <andy1116286@gmail.com>
2018-11-04 11:11:00 +0300

    [+] ApproximationGEMModelRedesigned

--- groupingEstimates.py
+++ groupingEstimates.py
@@ -181,8 +181,165 @@ class ApproximationGEMModel:
         return without_classification, with_classification
 
 
+class ApproximationGEMModelRedesigned(ApproximationGEMModel):
+    def __init__(self, exogen_data, endogen_data):
+        super(ApproximationGEMModelRedesigned, self).__init__(exogen_data, endogen_data)
+        self._segment_count_in_every_array = self._k_every_segment
+        self._np_freq_positive = None
+        self._np_freq_negative = None
+        self._np_freq_positive_reclassified = None
+        self._np_freq_negative_reclassified = None
+
+    def _prob_func(self, x_i, y_i, mu_i, beta_hat, is_positive=True):
+        a_mu_i_plus_1 = float('nan')
+        a_mu_i = float('nan')
+
+        if is_positive:
+            if mu_i == self._k_every_segment:
+                return 0.5 * (1 + self.erf((self.intervals_right_bound - x_i * beta_hat) / (math.sqrt(2.0 * self.sigmasq))))
+            a_mu_i_plus_1 = mu_i * self.interval_length
+            a_mu_i = mu_i * self.interval_length - self.interval_length
+        else:
+            if mu_i == self._k_every_segment:
+                return 0.5 * (1 + self.erf((self.intervals_left_bound - x_i * beta_hat) / (math.sqrt(2.0 * self.sigmasq))))
+            a_mu_i_plus_1 = -mu_i * self.interval_length
+            a_mu_i = -mu_i * self.interval_length - self.interval_length
+
+        return 0.5 * (self.erf((a_mu_i_plus_1 - x_i * beta_hat) / (math.sqrt(2.0 * self.sigmasq))) - self.erf((a_mu_i - x_i * beta_hat) / (math.sqrt(2.0 * self.sigmasq))))
+
+    def _dprob_func(self, x_i, y_i, mu_i, beta_hat, is_positive=True):
+        a_mu_i_plus_1 = float('nan')
+        a_mu_i = float('nan')
+        if is_positive:
+            if mu_i == self._k_every_segment:
+                return 2.0 * x_i * self.derf((self.intervals_right_bound - x_i * beta_hat) / (math.sqrt(2.0 * self.sigmasq))) / (1.0 + self.erf((self.intervals_left_bound - x_i * beta_hat) / (math.sqrt(2.0 * self.sigmasq)))).item((0, 0))
+
+            a_mu_i_plus_1 = mu_i * self.interval_length
+            a_mu_i = mu_i * self.interval_length - self.interval_length
+
+        else:
+            if mu_i == self._k_every_segment:
+                return 2.0 * x_i * self.derf((self.intervals_left_bound - x_i * beta_hat) / (math.sqrt(2.0 * self.sigmasq))) / (1.0 + self.erf((self.intervals_left_bound - x_i * beta_hat) / (math.sqrt(2.0 * self.sigmasq)))).item((0, 0))
+
+            a_mu_i_plus_1 = -mu_i * self.interval_length
+            a_mu_i = -mu_i * self.interval_length - self.interval_length
+
+        if self.erf((a_mu_i_plus_1 - x_i * beta_hat) / (math.sqrt(2.0 * self.sigmasq))) - self.erf(
+                (a_mu_i - x_i * beta_hat) / (math.sqrt(2.0 * self.sigmasq))) == 0.0:
+            return 0
+
+        temp = 0.0
+        temp += self.derf((a_mu_i_plus_1 - x_i * beta_hat) / (math.sqrt(2.0 * self.sigmasq))) - self.derf(
+            (a_mu_i - x_i * beta_hat) / (math.sqrt(2.0 * self.sigmasq)))
+        temp /= self.erf((a_mu_i_plus_1 - x_i * beta_hat) / (math.sqrt(2.0 * self.sigmasq))) - self.erf(
+            (a_mu_i - x_i * beta_hat) / (math.sqrt(2.0 * self.sigmasq)))
+        return 2.0 * x_i * temp.item((0, 0))
+
+    def _likelihood_f(self, beta, mu_data, is_positive=True):
+        current_likelihood_result = 0.0
+
+        for i in range(0, self.endogen.size):
+            if mu_data[i] is not None:
+                current_likelihood_result += np.log(self._prob_func(self.exogen[i], self.endogen[i], mu_data[i], beta, is_positive))
+
+        return current_likelihood_result
+
+    def _dlikelihood_f(self, beta, mu_data, is_positive=True):
+        current_likelihood_derivative_result = np.zeros(self.exogen[0].size)
+
+        for i in range(0, self.endogen.size):
+            if mu_data[i] is not None:
+                current_likelihood_derivative_result += self._dprob_func(self.exogen[i], self.endogen[i], mu_data[i], beta, is_positive)
+
+        return -0.5 * current_likelihood_derivative_result
+
+    def classify(self):
+        self._np_freq_positive = [None for i in range(self.endogen.size)]
+        self._np_freq_negative = [None for i in range(self.endogen.size)]
+
+        for i in range(self.endogen.size):
+            if self.endogen[i] >= 0:
+                self._np_freq_positive[i] = int(self.endogen[i] / self.interval_length)
+            else:
+                self._np_freq_negative[i] = int(abs(self.endogen[i]) / self.interval_length)
+
+        print("classified")
+
+        return self
+
+    def reclassify(self, delta):
+        self._np_freq_positive_reclassified = [None for i in range(self.endogen.size)]
+        self._np_freq_negative_reclassified = [None for i in range(self.endogen.size)]
+
+        for i in range(0, self.endogen.size):
+            current_faced_classes_positive = {}
+            current_faced_classes_negative = {}
+            for j in range(0, self.endogen.size):
+                if np.linalg.norm(self.exogen[i]-self.exogen[j]) <= delta:
+                    if self._np_freq_positive[j] is not None:
+                        if self._np_freq_positive[j] in current_faced_classes_positive:
+                            current_faced_classes_positive[self._np_freq_positive[j]] += 1
+                        else:
+                            current_faced_classes_positive[self._np_freq_positive[j]] = 1
+                    elif self._np_freq_negative[j] is not None:
+                        if self._np_freq_negative[j] in current_faced_classes_positive:
+                            current_faced_classes_negative[self._np_freq_negative[j]] += 1
+                        else:
+                            current_faced_classes_negative[self._np_freq_negative[j]] = 1
+                    else:
+                        continue
+            # TODO: продумать переклассификацию
+            maximumfacedtimes_positive = 1 if self._np_freq_positive[i] is not None else 0
+            maximumfacedclass_positive = self._np_freq_positive[i]
+            maximumfacedtimes_negative = 1 if self._np_freq_negative[i] is not None else 0
+            maximumfacedclass_negative = self._np_freq_negative[i]
+
+            for key in current_faced_classes_positive:
+                if maximumfacedtimes_positive < current_faced_classes_positive[key]:
+                    maximumfacedtimes_positive = current_faced_classes_positive[key]
+                    maximumfacedclass_positive = key
+
+            for key in current_faced_classes_negative:
+                if maximumfacedtimes_negative < current_faced_classes_negative[key]:
+                    maximumfacedtimes_negative = current_faced_classes_negative[key]
+                    maximumfacedclass_negative = key
+
+            if maximumfacedclass_positive > maximumfacedtimes_negative:
+                self._np_freq_positive_reclassified[i] = maximumfacedclass_positive
+            else:
+                self._np_freq_negative_reclassified[i] = maximumfacedclass_negative
+
+        print("reclassified")
+
+    def fit(self):
+        self.classify()
+        self.reclassify(0.5)
+
+        print("fitting.....")
+
+        beta_hat = np.matrix(np.ones(self.exogen[0].size)).T
+        beta_hat_next = np.matrix(np.zeros(self.exogen[0].size)).T
+
+        while np.linalg.norm(self._dlikelihood_f(beta_hat_next, self._np_freq_positive_reclassified, is_positive=True) + self._dlikelihood_f(beta_hat_next, self._np_freq_negative_reclassified, is_positive=False)) >= 0.1:
+            dlikelihood_f_for_beta_hat = self._dlikelihood_f(beta_hat, self._np_freq_positive_reclassified, is_positive=True) + self._dlikelihood_f(beta_hat, self._np_freq_negative_reclassified, is_positive=False)
+            dlikelihood_f_for_beta_hat_next = self._dlikelihood_f(beta_hat_next, self._np_freq_positive_reclassified, is_positive=True) + self._dlikelihood_f(beta_hat_next, self._np_freq_negative_reclassified, is_positive=False)
+            delta_beta = np.matrix(np.zeros(self.exogen[0].size)).T
+
+            for i in range(self.exogen[0].size):
+                delta_beta[i] = -dlikelihood_f_for_beta_hat_next[i] / (
+                            dlikelihood_f_for_beta_hat_next[i] - dlikelihood_f_for_beta_hat[i]) * (
+                                            beta_hat_next[i] - beta_hat[i])
+            beta_hat = beta_hat_next
+            beta_hat_next = beta_hat_next + delta_beta
+
+        return beta_hat_next
+
+    def fit_without_classification(self):
+        pass
+
+
 def GEM(exogen_data, endogen_data, *args):
-    return ApproximationGEMModel(exogen_data, endogen_data)
+    return ApproximationGEMModelRedesigned(exogen_data, endogen_data)
 
 
 def modulateRegression(regression_sample_quintity, regression_outlier_percentage):
@@ -204,7 +361,7 @@ def modulateRegression(regression_sample_quintity, regression_outlier_percentage
 x_points, y_points = modulateRegression(100, OUTLIER_PERCENTAGE)
 
 APPROXIMATION_MODEL = GEM(x_points,y_points)
-print(APPROXIMATION_MODEL.fit_without_classification())
+print(APPROXIMATION_MODEL.fit())
 
 APPROXIMATION_MODEL = sm.RLM(y_points,x_points, M=sm.robust.norms.HuberT())
 tempHundredParams = APPROXIMATION_MODEL.fit().params
