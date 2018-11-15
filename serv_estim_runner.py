dbdf904c897d718214ef81b56f5a8d50f14177da
Andrew Rumiantsev <andy1116286@gmail.com>
2018-11-15 22:23:05 +0300

    added timeouts

--- serv_estim_runner.py
+++ serv_estim_runner.py
@@ -2,11 +2,17 @@ import random
 
 import numpy as np
 import requests
+import signal
 
 from py_grouping_estimates import groupingEstimates
 
 ACCURATE_RESULT = np.matrix([90, 4]).T
 OUTLIER_PERCENTAGE = 8.0
+SECONDS_TIMEOUT = 60 * 5
+
+
+def alarm_handler(signum, frame):
+    raise Exception("function timeout")
 
 
 def modulateRegression(regression_sample_quintity, regression_outlier_percentage):
@@ -25,16 +31,22 @@ def modulateRegression(regression_sample_quintity, regression_outlier_percentage
     return _x_points, _y_points
 
 
+signal.signal(signal.SIGALRM, alarm_handler)
 while True:
     x_points, y_points = modulateRegression(100, OUTLIER_PERCENTAGE)
     APPROXIMATION_MODEL = groupingEstimates.GEM(x_points, y_points)
 
     try:
+        signal.alarm(SECONDS_TIMEOUT)
+
         result = "GEM {}".format(APPROXIMATION_MODEL.fit())
+        signal.alarm(0)
+
         data_for_req = {
             "run_res": result
         }
         req = requests.post(url='https://py-cw-results.herokuapp.com/pushRunResult', json=data_for_req)
+
         print("%s: %s: %s:" % (result, req.status_code, req.reason))
     except Exception as e:
         print("Exception occurred: %s" % e)
