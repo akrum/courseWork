1c9690a727ddc41bd7b9b36f73488f302a05e161
Andrew Rumiantsev <andy1116286@gmail.com>
2018-12-03 21:57:38 +0300

    [fix] process hang

--- serv_estim_runner.py
+++ serv_estim_runner.py
@@ -61,3 +61,6 @@ if __name__ == "__main__":
         action_process.join(timeout=SECONDS_TIMEOUT)
 
         action_process.terminate()
+
+        if action_process.is_alive():
+            action_process.terminate()
