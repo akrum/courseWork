8ea4a0f87a88f126435623d9656010fbf36306ee
Andrew Rumiantsev <andy1116286@gmail.com>
2018-12-09 18:06:47 +0300

    [~] plot_runner three vars

--- python_experiment/plot_runner.py
+++ python_experiment/plot_runner.py
@@ -1,16 +1,20 @@
 import random
 import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 from py_grouping_estimates import groupingEstimates
 
 
-ACCURATE_RESULT = np.matrix([90, 4]).T
+ACCURATE_RESULT = np.matrix([90, 4, 7]).T
 OUTLIER_PERCENTAGE = 8.0
-SAMPLE_SIZE_STEP = 10
-PLOT_SIZE = 100
-MEANING_FACTOR = 100
+SAMPLE_SIZE = 100
+PLOT_SIZE = 20
+
+
+def alarm_handler(signum, frame):
+    raise Exception("function timeout")
 
 
 def modulate_regression(regression_sample_quintity, regression_outlier_percentage):
@@ -30,28 +34,43 @@ def modulate_regression(regression_sample_quintity, regression_outlier_percentag
 
 
 if __name__ == "__main__":
-    differences = []
-    sample_sizes = []
-    for i in range(0, PLOT_SIZE):
+    first_coordinates_with_classification = []
+    second_coordinates_with_classification = []
+    third_coordinates_with_classification = []
+    first_coordinates_without_classification = []
+    second_coordinates_without_classification = []
+    third_coordinates_without_classification = []
+
+    for iter_time in range(0, PLOT_SIZE):
         try:
-            this_samle_size = 80 + i * SAMPLE_SIZE_STEP
-            x_points, y_points = modulate_regression(this_samle_size, OUTLIER_PERCENTAGE)
+            x_points, y_points = modulate_regression(SAMPLE_SIZE, OUTLIER_PERCENTAGE)
 
             APPROXIMATION_MODEL = groupingEstimates.GEM(x_points, y_points)
-            t_result = APPROXIMATION_MODEL.fit()
+            t_result_without = APPROXIMATION_MODEL.fit_without_reclassification()
 
-            differences.append(np.linalg.norm(t_result-ACCURATE_RESULT))
-            sample_sizes.append(this_samle_size)
+            APPROXIMATION_MODEL = groupingEstimates.GEM(x_points, y_points)
+            t_result_with = APPROXIMATION_MODEL.fit()
+
+            first_coordinates_without_classification.append(t_result_without[0])
+            second_coordinates_without_classification.append(t_result_without[1])
+            third_coordinates_without_classification.append(t_result_without[2])
+
+            first_coordinates_with_classification.append(t_result_with[0])
+            second_coordinates_with_classification.append(t_result_with[1])
+            third_coordinates_with_classification.append(t_result_with[2])
         except np.linalg.linalg.LinAlgError as e:
             print(e)
         except StopIteration as e:
             print(e)
 
-
-plt.title("Зависимость точности от размера выборки")
-plt.xlabel("размер выборки")
-plt.ylabel("точность")
-# plt.axis([80, 100, 3, 5])
-
-sns.lineplot(sample_sizes, differences)
-plt.show()
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.set_title("Оценки вектора [90, 4, 7]")
+    ax.set_xlabel("beta_0")
+    ax.set_ylabel("beta_1")
+    ax.set_zlabel("beta_2")
+    # ax.set_ax([80, 100, 3, 5, 6, 8])
+    ax.scatter(first_coordinates_without_classification, second_coordinates_without_classification, third_coordinates_without_classification, color="green")
+    ax.scatter(first_coordinates_with_classification, second_coordinates_with_classification, third_coordinates_with_classification, color="blue")
+    ax.scatter(list(ACCURATE_RESULT[0]), list(ACCURATE_RESULT[1]), list(ACCURATE_RESULT[2]), color="red")
+    plt.show()
