20b5b03a9ad21d707e2c4852d59602dbe8167c02
Andrew Rumiantsev <andy1116286@gmail.com>
2019-02-27 22:21:44 +0300

    [+] added groupingEstimatesNaive that are using means of classes

---
+++ python_experiment/py_grouping_estimates/groupingEstimatesNaive.py
@@ -0,0 +1,26 @@
+from .groupingEstimates import ApproximationGEMModelRedesigned
+from .GroupingEstimatesDefines import GroupingEstimatesDefines as Defines
+
+import numpy as np
+
+
+class ApproximationGEMModelNaive(ApproximationGEMModelRedesigned):
+    def fit(self):
+        self.classify()
+
+        def ex_generator(mu_data):
+            for i in range(0, self.endogen.size):
+                if mu_data[i] is None:
+                    continue
+                a_mu_i_plus_1 = mu_data[i] * Defines.INTERVAL_LENGTH
+                a_mu_i = mu_data[i] * Defines.INTERVAL_LENGTH - Defines.INTERVAL_LENGTH
+                yield (a_mu_i_plus_1 + a_mu_i) / 2
+
+        naive_ex_data_positive = np.fromiter(ex_generator(self._np_freq_positive), float)
+        naive_ex_data_negative = np.fromiter(ex_generator(self._np_freq_negative), float)
+
+        naive_ex_data_full = np.append(naive_ex_data_positive, naive_ex_data_negative)
+
+        z, resid, rank, sigma = np.linalg.lstsq(self.exogen, naive_ex_data_full, rcond=None)
+        return z
+
