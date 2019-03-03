9c596e6185c1c93c3cda8cbc10c145eaebc141e8
Andrew Rumiantsev <andy1116286@gmail.com>
2019-03-03 18:24:37 +0300

    [~] added fitting retry to big_data_runner

--- python_experiment/big_data_runner.py
+++ python_experiment/big_data_runner.py
@@ -10,7 +10,7 @@ ACCURATE_RESULT = np.matrix([90, 4]).T
 OUTLIER_PERCENTAGE = 8.0
 SECONDS_TIMEOUT = 60 * 5
 SAMPLE_SIZE_MIN = 100
-SAMPLE_SIZE_MAX = 2000
+SAMPLE_SIZE_MAX = 5000
 SAMPLE_SIZE_STEP = 100
 np.seterr(all='raise')
 NP_DATA_PATH = "./np_data_created/"
@@ -41,25 +41,30 @@ def fit_data_naive_classic():
     all_results_classic = []
     all_results_naive = []
     for sample_size in range(SAMPLE_SIZE_MIN, SAMPLE_SIZE_MAX+1, SAMPLE_SIZE_STEP):
-        x_points, y_points = modulateRegression(sample_size, OUTLIER_PERCENTAGE)
-        approx_model = groupingEstimates.GEM(x_points, y_points)
-        approx_model_naive = groupingEstimatesNaive.GEM_N(x_points, y_points)
-        try:
-            result = approx_model.fit()
-            print("GEM {}".format(result))
-            result_naive = approx_model_naive.fit()
-            print("GEM_N {}".format(result_naive))
-            all_results_classic.append(result)
-            all_results_naive.append(result_naive)
-            sample_sizes.append(sample_size)
-        except KeyboardInterrupt:
-            print("stopping...")
-            np.save(NP_DATA_PATH + "gem_res_classic", all_results_classic)
-            np.save(NP_DATA_PATH + "gem_res_naive", all_results_naive)
-            np.save(NP_DATA_PATH + "gem_sizes", sample_sizes)
-            quit()
-        except Exception as e:
-            print(e)
+        successful_fit = False
+        while not successful_fit:
+            x_points, y_points = modulateRegression(sample_size, OUTLIER_PERCENTAGE)
+            approx_model = groupingEstimates.GEM(x_points, y_points)
+            approx_model_naive = groupingEstimatesNaive.GEM_N(x_points, y_points)
+            try:
+                result = approx_model.fit()
+                print("GEM {}".format(result))
+                result_naive = approx_model_naive.fit()
+                print("GEM_N {}".format(result_naive))
+
+                successful_fit = True
+
+                all_results_classic.append(result)
+                all_results_naive.append(result_naive)
+                sample_sizes.append(sample_size)
+            except KeyboardInterrupt:
+                print("stopping...")
+                np.save(NP_DATA_PATH + "gem_res_classic", all_results_classic)
+                np.save(NP_DATA_PATH + "gem_res_naive", all_results_naive)
+                np.save(NP_DATA_PATH + "gem_sizes", sample_sizes)
+                quit()
+            except Exception as e:
+                print(e)
     np.save(NP_DATA_PATH + "gem_res_classic", all_results_classic)
     np.save(NP_DATA_PATH + "gem_res_naive", all_results_naive)
     np.save(NP_DATA_PATH + "gem_sizes", sample_sizes)
@@ -70,24 +75,29 @@ def plot_with_different_sample_size():
     all_results_with_classification = []
     all_results_without_classification = []
     for sample_size in range(SAMPLE_SIZE_MIN, SAMPLE_SIZE_MAX+1, SAMPLE_SIZE_STEP):
-        x_points, y_points = modulateRegression(sample_size, OUTLIER_PERCENTAGE)
-        approx_model = groupingEstimates.GEM(x_points, y_points)
-        try:
-            result = approx_model.fit()
-            print("GEM {}".format(result))
-            result_without = approx_model.fit_without_reclassification()
-            print("GEM_without {}".format(result_without))
-            all_results_with_classification.append(result)
-            all_results_without_classification.append(result_without)
-            sample_sizes.append(sample_size)
-        except KeyboardInterrupt:
-            print("stopping...")
-            np.save(NP_DATA_PATH + "gem_res_with", all_results_with_classification)
-            np.save(NP_DATA_PATH + "gem_res_without", all_results_without_classification)
-            np.save(NP_DATA_PATH + "gem_sizes_with_without", sample_sizes)
-            quit()
-        except Exception as e:
-            print(e)
+        successful_fit = False
+        while not successful_fit:
+            x_points, y_points = modulateRegression(sample_size, OUTLIER_PERCENTAGE)
+            approx_model = groupingEstimates.GEM(x_points, y_points)
+            try:
+                result = approx_model.fit()
+                print("GEM {}".format(result))
+                result_without = approx_model.fit_without_reclassification()
+                print("GEM_without {}".format(result_without))
+
+                successful_fit = True
+
+                all_results_with_classification.append(result)
+                all_results_without_classification.append(result_without)
+                sample_sizes.append(sample_size)
+            except KeyboardInterrupt:
+                print("stopping...")
+                np.save(NP_DATA_PATH + "gem_res_with", all_results_with_classification)
+                np.save(NP_DATA_PATH + "gem_res_without", all_results_without_classification)
+                np.save(NP_DATA_PATH + "gem_sizes_with_without", sample_sizes)
+                quit()
+            except Exception as e:
+                print(e)
     np.save(NP_DATA_PATH + "gem_res_with", all_results_with_classification)
     np.save(NP_DATA_PATH + "gem_res_without", all_results_without_classification)
     np.save(NP_DATA_PATH + "gem_sizes_with_without", sample_sizes)
