91116f0bf162bf838d42c47d31fa7b14847b492c
Andrew Rumiantsev <andy1116286@gmail.com>
2019-05-19 17:08:49 +0300

    [~] more theory, more images

--- python_experiment/plot_runner.py
+++ python_experiment/plot_runner.py
@@ -2,15 +2,18 @@ import random
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
-import seaborn as sns
-import big_data_runner
+import statsmodels.api as sm
+
 
 from py_grouping_estimates import groupingEstimates
 
 
-ACCURATE_RESULT = np.matrix([90, 4, 7]).T
+ACCURATE_RESULT = np.matrix([90, 4]).T
 OUTLIER_PERCENTAGE = 8.0
 SAMPLE_SIZE = 100
+SAMPLE_SIZE_MIN = 100
+SAMPLE_SIZE_MAX = 1000
+SAMPLE_SIZE_STEP = 10
 PLOT_SIZE = 30
 
 
@@ -87,5 +90,100 @@ def with_without_reclassification():
     plt.show()
 
 
+def m_estimators():
+    sample_sizes = []
+    all_results_m_estim = []
+
+    x_points = None
+    y_points = None
+
+    def _visualize():
+        plt.title("Оценки параметров линейной регрессии c искажениями (М-оценки)")
+        plt.xlabel("размер выборки")
+        plt.ylabel("вариации оценок")
+
+        def generate_differences(_tarray):
+            for item in _tarray:
+                yield np.linalg.norm(ACCURATE_RESULT.A1 - item)
+
+        plt.axis([0, sample_sizes[-1], 0, 10.0])
+
+        plot_without, = plt.plot(sample_sizes,
+                                 list(generate_differences(all_results_m_estim)),
+                                 color="red")
+        plt.show()
+
+    for sample_size in range(SAMPLE_SIZE_MIN, SAMPLE_SIZE_MAX + 1, SAMPLE_SIZE_STEP):
+        print("fitting with sample size: {}".format(sample_size))
+        successful_fit = False
+        while not successful_fit:
+            x_points_t, y_points_t = modulate_regression(SAMPLE_SIZE_STEP, OUTLIER_PERCENTAGE)
+
+            if x_points is None or y_points is None:
+                x_points, y_points = modulate_regression(SAMPLE_SIZE_MIN, OUTLIER_PERCENTAGE)
+            else:
+                x_points = np.append(x_points, x_points_t, axis=0)
+                y_points = np.append(y_points, y_points_t, axis=0)
+
+            approx_model = sm.RLM(y_points, x_points, M=sm.robust.norms.HuberT())
+            try:
+                result_without = approx_model.fit().params
+                print("RLM {}".format(result_without))
+                successful_fit = True
+
+                all_results_m_estim.append(result_without)
+                sample_sizes.append(sample_size)
+            except KeyboardInterrupt:
+                print("stopping...")
+                _visualize()
+                quit()
+            except Exception as e:
+                print(e)
+
+    _visualize()
+
+
+def ols_estimators():
+    sample_sizes = []
+    all_differences_m_estim = []
+
+    x_points = None
+    y_points = None
+
+    for sample_size in range(SAMPLE_SIZE_MIN, SAMPLE_SIZE_MAX + 1, SAMPLE_SIZE_STEP):
+        print("fitting with sample size: {}".format(sample_size))
+        x_points_t, y_points_t = modulate_regression(SAMPLE_SIZE_STEP, OUTLIER_PERCENTAGE)
+
+        full_dif = 0.0
+
+        for i in range(0, 100):
+            if x_points is None or y_points is None:
+                x_points, y_points = modulate_regression(SAMPLE_SIZE_MIN, OUTLIER_PERCENTAGE)
+            else:
+                x_points_t = np.append(x_points, x_points_t, axis=0)
+                y_points_t = np.append(y_points, y_points_t, axis=0)
+
+            approx_model = sm.OLS(y_points_t, x_points_t, M=sm.robust.norms.HuberT())
+
+            result_without = approx_model.fit().params
+            print("RLM {}".format(result_without))
+            full_dif += np.linalg.norm(ACCURATE_RESULT.A1 - result_without)
+
+        full_dif /= 100
+        all_differences_m_estim.append(full_dif)
+        sample_sizes.append(sample_size)
+
+    plt.title("Оценки параметров линейной регрессии c искажениями (МНК-оценки)")
+    plt.xlabel("размер выборки")
+    plt.ylabel("вариации оценок")
+
+    plt.axis([0, sample_sizes[-1], 0, 10.0])
+
+    plot_without, = plt.plot(sample_sizes,
+                             all_differences_m_estim,
+                             color="red")
+    plt.show()
+
+
 if __name__ == "__main__":
-    with_without_reclassification()
+    ols_estimators()
